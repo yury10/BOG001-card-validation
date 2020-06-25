640a4efc2c6818a0c9090789f124db3be04d8340
Angela Castro <kimypunchos@hotmail.com>
2020-06-25 13:15:57 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,40 @@
+import validator from './validator.js';
+let nombre = document.getElementById('nombre');
+let creditCardNumber = document.getElementById('creditCardNomber')
+document.getElementById('validarBoton').addEventListener("click", validar);
+
+nombre.addEventListener('keyup', (e) => {// (e) es un objeto de tipo evento
+   //console.log(e);
+   let nombreInput = e.target.value;
+   nombre.value = nombreInput
+   .replace (/[0-9]/g, '')//Nos elimina los numeros
+});
+
+creditCardNumber.addEventListener("keyup",(e)=> {// keyup para que realice funcion cada que se presiona una tecla
+   //console.log(e);
+   let numeroTarjeta= e.target.value;
+   creditCardNumber.value= numeroTarjeta
+   .replace(/\s/g, '')//Eliminamos espacios en blanco (es una expresion)
+   .replace(/\D/g, '')// Elimina las letras
+	.trim();//Elimina el ultimo espacio en blanco
+});
+
+//console.log(tarjeta);
+function validar(){
+   let numeroTarjeta = document.getElementById('creditCardNumber').value;
+   //console.log('hola'); //lo usamos para probar el boton
+   if (numeroTarjeta === '') {
+      alert('Estimado usuario, por favor digite su nùmero de Tarjeta de Crédito');
+   }else{
+      let valida = validator.isValid(numeroTarjeta);
+      if (valida) {
+         document.getElementById('creditCardNumber');
+         alert('Estimado usuario su Tarjeta de Crédito es VALIDA');
+      } else{
+         document.getElementById("creditCardNumber");
+         alert('Estimado usuario su Tarjeta de Crédito es  NO VALIDA');
+      }
+   }
+   let numeroEnmascarado = validator.maskify(numeroTarjeta);
+   console.log(numeroEnmascarado);
+}
